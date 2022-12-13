1eee8d7b9c6e0cc414a61a89d2cbb9d307b0c8c4
Abubakar Ummar <105272189+Haywayaheadshot@users.noreply.github.com>
2022-12-13 16:27:39 -0600

    Correct typing error

--- queries.sql
+++ queries.sql
@@ -1,7 +1,5 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-/*Queries that provide answers to the questions from all projects.*/
-
 SELECT * from animals WHERE name like '%mon';
 select name from animals where date_of_birth between '2016-01-01' and '2019-12-31';
 select name from animals where neutered = true and escape_attempts < 3;
