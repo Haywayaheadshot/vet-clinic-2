801b56bf085d49a01608f6cdb106c02950de360e
Abubakar Ummar <105272189+Haywayaheadshot@users.noreply.github.com>
2022-12-13 16:33:17 -0600

    Change commands to small letters

--- queries.sql
+++ queries.sql
@@ -1,6 +1,6 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name like '%mon';
+select * from animals where name like '%mon';
 select name from animals where date_of_birth between '2016-01-01' and '2019-12-31';
 select name from animals where neutered = true and escape_attempts < 3;
 select date_of_birth from animals where name in ('Agumon','Pikachu');
