268551af0e86d92749ee267ef41706df29568ec2
Abubakar Ummar <105272189+Haywayaheadshot@users.noreply.github.com>
2022-12-14 13:45:09 -0600

    Add more data to data.sql

--- data.sql
+++ data.sql
@@ -1,6 +1,16 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals ("id","name","date_of_birth","escape_attempts","neutered","weigth_kg") VALUES (1, "Agumon", "2020-02-03", 0, True, 10.23);
-INSERT INTO animals ("id","name","date_of_birth","escape_attempts","neutered","weigth_kg") VALUES (2, "Gabumon", "2018-11-15", 2, True, 8);
-INSERT INTO animals ("id","name","date_of_birth","escape_attempts","neutered","weigth_kg") VALUES (3, "Pikachu", "2021-01-07", 1, False, 15.04);
-INSERT INTO animals ("id","name","date_of_birth","escape_attempts","neutered","weigth_kg") VALUES (4, "Devimon", "2017-05-12", 5, True, 11);
+INSERT INTO animals ("id","name","date_of_birth","escape_attempts","neutered","weigth_kg") VALUES (1, 'Agumon', "2020-02-03", 0, True, 10.23);
+INSERT INTO animals ("id","name","date_of_birth","escape_attempts","neutered","weigth_kg") VALUES (2, 'Gabumon', "2018-11-15", 2, True, 8);
+INSERT INTO animals ("id","name","date_of_birth","escape_attempts","neutered","weigth_kg") VALUES (3, 'Pikachu', "2021-01-07", 1, False, 15.04);
+INSERT INTO animals ("id","name","date_of_birth","escape_attempts","neutered","weigth_kg") VALUES (4, 'Devimon', "2017-05-12", 5, True, 11);
+
+/* Update database */
+
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Charmander', '2020-02-08', 0, false, 11);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Plantmon', '2021-11-15', 2, true, 5.7);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Squirtle', '1993-04-02', 3, false, 12.13);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Angemon', '2005-06-12', 1, true, 45);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Boarmon', '2005-06-07', 7, true, 20.4);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Blossom', '1998-10-13', 3, true, 17);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Ditto', '2022-05-14', 4, true, 22);
