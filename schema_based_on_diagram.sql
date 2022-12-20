6f6c37775eb49c7552cfdb30c08f816b55cb86dd
Abubakar Ummar <105272189+Haywayaheadshot@users.noreply.github.com>
2022-12-20 10:25:48 -0600

    Create database

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE "create-db-based-on-diagram"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+    
+    
